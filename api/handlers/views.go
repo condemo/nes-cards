3de43de2d9a0d29b882a9fcb167111beb6b91f17
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-01-01 21:34:15 +0100

    feat: added basic routes

--- api/handlers/views.go
+++ api/handlers/views.go
@@ -14,8 +14,26 @@ func NewViewsHandler() *viewsHandler {
 
 func (h *viewsHandler) RegisterRoutes(r *http.ServeMux) {
 	r.HandleFunc("GET /", h.homeView)
+	r.HandleFunc("GET /welcome", h.welcomeView)
+	r.HandleFunc("GET /game", h.gameView)
+	r.HandleFunc("GET /history", h.historyView)
 }
 
 func (h *viewsHandler) homeView(w http.ResponseWriter, r *http.Request) {
 	RenderTempl(w, r, core.Home())
+}
+
+func (h *viewsHandler) welcomeView(w http.ResponseWriter, r *http.Request) {
+	// TODO:
+	RenderTempl(w, r, core.WelcomeView())
+}
+
+func (h *viewsHandler) gameView(w http.ResponseWriter, r *http.Request) {
+	// TODO:
+	RenderTempl(w, r, core.GameView())
+}
+
+func (h *viewsHandler) historyView(w http.ResponseWriter, r *http.Request) {
+	// TODO:
+	RenderTempl(w, r, core.HistoryView())
 }
