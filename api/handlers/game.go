474415809f91686744da2fd5346a12bcadf872b3
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-01-02 01:19:11 +0100

    feat: added mockup data from server

--- api/handlers/game.go
+++ api/handlers/game.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/condemo/nes-cards/public/views/core"
+	"github.com/condemo/nes-cards/types"
 )
 
 type gameHandler struct{}
@@ -23,6 +25,47 @@ func (h gameHandler) newGameView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h gameHandler) newGamePost(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("post made")
-	RenderTempl(w, r, core.GameView())
+	var playerHP uint8
+	var towerHP uint8
+	p1 := r.FormValue("player1")
+	p2 := r.FormValue("player2")
+
+	pHP := r.FormValue("player-hp")
+	if pHP == "" {
+		playerHP = 80
+	} else {
+		p, err := strconv.ParseUint(pHP, 0, 8)
+		if err != nil {
+			log.Fatal(err)
+		}
+		playerHP = uint8(p)
+	}
+
+	tHP := r.FormValue("tower-hp")
+	if tHP == "" {
+		towerHP = 60
+	} else {
+		t, err := strconv.ParseUint(pHP, 0, 8)
+		if err != nil {
+			log.Fatal(err)
+		}
+		towerHP = uint8(t)
+	}
+
+	// pHP, err := strconv.ParseUint(r.FormValue("player-hp"), 0, 8)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// tHP, err := strconv.ParseUint(r.FormValue("tower-hp"), 0, 8)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	player1 := types.NewPlayer(p1, uint8(playerHP))
+	player2 := types.NewPlayer(p2, uint8(playerHP))
+	tower1 := types.NewTower(uint8(towerHP))
+	tower2 := types.NewTower(uint8(towerHP))
+
+	RenderTempl(w, r, core.GameView(player1, player2, tower1, tower2))
 }
--- api/handlers/views.go
+++ api/handlers/views.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/condemo/nes-cards/public/views/core"
+	"github.com/condemo/nes-cards/types"
 )
 
 type viewsHandler struct{}
@@ -24,7 +25,12 @@ func (h *viewsHandler) homeView(w http.ResponseWriter, r *http.Request) {
 
 func (h *viewsHandler) gameView(w http.ResponseWriter, r *http.Request) {
 	// TODO:
-	RenderTempl(w, r, core.GameView())
+	p1 := types.NewPlayer("Demo1", 80)
+	p2 := types.NewPlayer("Demo2", 80)
+	t1 := types.NewTower(60)
+	t2 := types.NewTower(60)
+
+	RenderTempl(w, r, core.GameView(p1, p2, t1, t2))
 }
 
 func (h *viewsHandler) historyView(w http.ResponseWriter, r *http.Request) {
