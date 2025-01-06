5c5618037bf82d5a1031dcd6970dcd206a4e54ed
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-01-06 20:54:58 +0100

    min size tweak

--- public/views/core/home_templ.go
+++ public/views/core/home_templ.go
@@ -43,7 +43,7 @@ func Home() templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"main-home\" class=\"p-4\"><div id=\"main-view\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"main-home\" class=\"p-4 pb-20\"><div id=\"main-view\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
